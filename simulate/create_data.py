3cd9ab47ef34fc46bc82eec957d8acdee77d289a
Varrial <73936910+Varrial@users.noreply.github.com>
2022-03-16 19:48:43 +0100

    4.1.1 - symulacja

--- simulate/create_data.py
+++ simulate/create_data.py
@@ -15,9 +15,13 @@ client = mqtt.Client("client1")  # create new instance
 client.connect(broker_address)  # connect to broker
 client.subscribe("hala1/maszyny/wielopila1")
 
+tmp = 0
+
 for i in lista:
     time.sleep(1)
-    client.publish("hala1/maszyny/wielopila1", json.dumps({'nazwa': "wielopila1", 'posuw_mb': i['PRAD'], 'posuw_skok': i['POSUW_KROK'], 'prad': i['POSUW'], 'zolty_sygn': i['ZOLTY'], 'zielony_sygn': i['ZIELONY'], 'impuls': 0}))
+    if tmp < 360:
+        tmp+=1
+        client.publish("hala1/maszyny/wielopila1", json.dumps({'nazwa': "wielopila1", 'posuw_mb': i['PRAD'], 'posuw_skok': i['POSUW_KROK'], 'prad': i['POSUW'], 'zolty_sygn': 1, 'zielony_sygn': 0, 'impuls': 0}))
+    else:
+        client.publish("hala1/maszyny/wielopila1", json.dumps({'nazwa': "wielopila1", 'posuw_mb': i['PRAD'], 'posuw_skok': i['POSUW_KROK'], 'prad': i['POSUW'], 'zolty_sygn': i['ZOLTY'], 'zielony_sygn': i['ZIELONY'], 'impuls': 0}))
     print("publikuje")
-
-(i['PRAD'], 0, i['POSUW'], 0, i['ZOLTY'], i['DATA_SERWER'])
